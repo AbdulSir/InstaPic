2087d545fded0999138971e31ba7fcb9858bc56c
ESEliot <60187997+ESEliot@users.noreply.github.com>
2020-03-20 15:09:55 -0400

    app.py
    It's used to set up the routes that will become the application's points of interaction

---
+++ Start/app.py
@@ -0,0 +1,25 @@
+app = Flask(__name__)
+Bootstrap(app)
+
+@app.route('/base_template.html')
+def base():
+    return render_template('base_template.html')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
