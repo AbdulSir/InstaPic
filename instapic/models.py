2c0d5e85a15393737d74d2f00ed901ed62a4afc1
ArsenyKokotov <52434320+ArsenyKokotov@users.noreply.github.com>
2020-03-30 14:30:44 -0400

    Creating Photo table in database
    #33, #35, #15

--- instapic/models.py
+++ instapic/models.py
@@ -7,3 +7,12 @@ class User(models.Model):
     email = models.CharField(max_length=140)
     last_login = models.DateTimeField(auto_now=True)
     profilepic = models.CharField(max_length=255, default="")
+
+class Photo(models.Model):
+	baseurl = models.CharField(max_length=255)
+	url = models.CharField(max_length=255)
+	date_uploaded = models.DateTimeField(auto_now=True)
+	owner = models.CharField(max_length=20)
+	likes = models.IntegerField()
+	caption = models.CharField(max_length=140, default="")
+	main_colour = models.CharField(max_length=15, default="")
