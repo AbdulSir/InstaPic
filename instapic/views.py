92206ad553097a3a23c58f2282abf751a169325b
ArsenyKokotov <52434320+ArsenyKokotov@users.noreply.github.com>
2020-03-30 14:34:19 -0400

    Saving photo function for HTML page
    #33, #35, #11, #15

--- instapic/views.py
+++ instapic/views.py
@@ -26,3 +26,9 @@ def ajaxlogin(request):
 	    login(request, logged_in_user) #use django method to login
 	context = {'ajax_output': output} #store variables to be put into template
 	return render(request, 'ajax.html', context)
+
+#saving a photo
+def ajaxsavephoto(request):
+	ajax = AjaxSavePhoto(request.POST, request.user)
+	context = { 'ajax_output': ajax.output() }
+	return render(request, 'ajax.html', context)
